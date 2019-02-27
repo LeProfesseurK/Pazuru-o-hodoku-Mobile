2cfef450268c3e1a74a0919a86a0e7e69465cdab
Nicolas0verd3nineji <verdiere.njl@gmail.com>
2019-02-27 15:02:01 +0100

    android vierge

---
+++ app/src/main/java/ccm/cours/nicolas/tiniki/ActivityMainActivity.java
@@ -0,0 +1,14 @@
+package ccm.cours.nicolas.tiniki;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_activity_main);
+        
+    }
+}
